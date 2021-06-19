cc090681831c4af99883157e3857a9081a58f9dd
haidousm <haidous.m@gmail.com>
2021-06-19 19:50:00 +0300

    refactor js

--- public/shell/assets/js/shell.js
+++ public/shell/assets/js/shell.js
@@ -1,94 +1,79 @@
 const history = [];
-const cmdSubmitted = (cmd) => {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            shellOutputLines = JSON.parse("[" + this.response + "]");
-            printResponse(cmd, shellOutputLines);
-        }
-    };
-    xhttp.open(
-        "POST",
-        "https://haidousm.com/shell/inc/php/handle_request.php",
-        true
-    );
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    formattedCmd = encodeURIComponent(cmd);
-    xhttp.send("cmd=" + formattedCmd);
-};
 
-const printResponse = (cmd, shellOutputLines) => {
-    for (var i = 0; i < shellOutputLines.length; i++) {
-        const pTag = document.createElement("p");
-        pTag.classList.add("ml-1");
-        pTag.innerHTML = shellOutputLines[i];
-        document
-            .getElementsByClassName("output-container")[0]
-            .appendChild(pTag);
-    }
-};
+$(document).ready(() => {
+    let cmdInput = $(".cmd-input");
+    cmdInput.trigger("focus");
+    cmdInput.trigger("select");
 
-document.getElementsByClassName("cmd-input")[0].onkeyup = function (e) {
+    $(cmdInput).on("keyup", (e) => inputChanged(e));
+    $(".terminal-container").on("click", () => {
+        let cmdInput = $(".cmd-input");
+        cmdInput.trigger("focus");
+        cmdInput.trigger("select");
+    });
+});
+
+const inputChanged = (e) => {
     if (e.key === "Enter" || e.keyCode === 13) {
         let cmd = e.target.value;
         const pTag = document.createElement("p");
-        pTag.innerHTML =
-            "root<span class='hidden md:inline'>@haidousm.com</span> $ " + cmd;
+        $(pTag).html(
+            `root<span class='hidden md:inline'>@haidousm.com</span> $ ${cmd}`
+        );
 
-        document
-            .getElementsByClassName("output-container")[0]
-            .appendChild(pTag);
-
-        updateScroll();
+        $(".output-container").append($(pTag));
 
         if (cmd.includes("sudo")) {
-            document
-                .getElementsByClassName("rick-container")[0]
-                .classList.remove("hidden");
-            document
-                .getElementsByClassName("terminal-container")[0]
-                .classList.add("hidden");
-            document.getElementById("rick").src =
-                "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&showinfo=0&controls=0&autohide=1";
+            $(".rick-container").removeClass("hidden");
+            $(".terminal-container").addClass("hidden");
+            $("#rick").attr(
+                "src",
+                "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&showinfo=0&controls=0&autohide=1"
+            );
         } else if (cmd == "clear") {
-            document.getElementsByClassName("output-container")[0].innerHTML =
-                "";
+            $(".output-container").html("");
             history.push(cmd);
         } else if (cmd == "history") {
-            printResponse(cmd, history);
+            printOutput(history);
         } else {
             cmdSubmitted(cmd);
             history.push(cmd);
         }
         e.target.value = "";
+        $(".terminal-container").scrollTop(
+            $(".terminal-container").height() + 1000
+        );
     }
 };
 
-function updateScroll() {
-    var element = document.getElementsByClassName("terminal-container")[0];
-    element.scrollTop = element.scrollHeight;
-}
+const printOutput = (output) => {
+    for (var i = 0; i < output.length; i++) {
+        const pTag = document.createElement("p");
+        $(pTag).addClass("ml-1");
+        $(pTag).html(output[i]);
+        $(".output-container").append($(pTag));
+    }
+    $(".terminal-container").scrollTop(
+        $(".terminal-container").height() + 1000
+    );
+};
 
-function closeRick() {
-    document
-        .getElementsByClassName("rick-container")[0]
-        .classList.add("hidden");
-    document
-        .getElementsByClassName("terminal-container")[0]
-        .classList.remove("hidden");
-    document.getElementById("rick").src = "";
-}
+const cmdSubmitted = (cmd) => {
+    let json_payload = { cmd: cmd };
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "https://haidousm.com/shell/inc/php/handle_request.php",
+        data: { json_payload },
+        success: (res) => {
+            printOutput(res.output);
+        },
+        error: console.error,
+    });
+};
 
-document.addEventListener("readystatechange", (event) => {
-    var cmdInput = document.getElementsByClassName("cmd-input")[0];
-    cmdInput.focus();
-    cmdInput.select();
-
-    document
-        .getElementsByClassName("terminal-container")[0]
-        .addEventListener("click", () => {
-            var cmdInput = document.getElementsByClassName("cmd-input")[0];
-            cmdInput.focus();
-            cmdInput.select();
-        });
-});
+const closeRick = () => {
+    $(".rick-container").addClass("hidden");
+    $(".terminal-container").removeClass("hidden");
+    $("#rick").src = "";
+};
