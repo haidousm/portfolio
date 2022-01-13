51c2f93b063f3f6199bbfad96362366f819dbe60
haidousm <haidous.m@gmail.com>
2022-01-13 17:38:43 +0200

    copy over structure and config from traefiker

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>Moussa Haidous</title>
+                <meta
+                    name="description"
+                    content="Moussa Haidous, Software Engineer"
+                />
+            </Head>
+            <main>
+                <h1>Hello</h1>
+            </main>
+        </div>
+    );
+};
+
+export default Home;
