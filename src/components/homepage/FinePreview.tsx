666934f6bc6f82ce80923f51c5ff260de1a4efbb
haidousm <haidous.m@gmail.com>
2022-01-28 13:39:40 +0200

    add todo to create fine preview later

--- src/components/homepage/FinePreview.tsx
+++ src/components/homepage/FinePreview.tsx
@@ -3,31 +3,10 @@ import ContainerOverlay from "../container-overlay/ContainerOverlay";
 import Terminal from "../terminal/Terminal";
 
 function FinePreview() {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        if (canvas) {
-            canvas.style.width = "100%";
-            canvas.style.height = "100%";
-            canvas.width = canvas.offsetWidth;
-            canvas.height = canvas.offsetHeight;
-            drawNumber(canvas, 1);
-        }
-    }, []);
-
-    const drawNumber = (canvas: HTMLCanvasElement, number: number) => {
-        const ctx = canvas.getContext("2d");
-        if (!ctx) {
-            return;
-        }
-        ctx.fillStyle = "white";
-        const image = new Image();
-    };
-
     return (
         <ContainerOverlay>
             <Terminal statusBarHTML={<Fragment> Predictions: -- </Fragment>}>
-                <canvas ref={canvasRef}></canvas>
+                <video autoPlay>{/* TODO: add gifs */}</video>
             </Terminal>
             <Fragment>
                 <h3 className="text-2xl text-white text-center">Fine</h3>
