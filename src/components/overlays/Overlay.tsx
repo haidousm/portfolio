0255506f3d638e9a107710357bacdab7520e9e2d
haidousm <haidous.m@gmail.com>
2022-01-28 15:40:06 +0200

    fix overlay

--- src/components/overlays/Overlay.tsx
+++ src/components/overlays/Overlay.tsx
@@ -7,20 +7,22 @@ function Overlay({ children, isVisible }: Props) {
     return (
         <div className="h-full w-full relative group">
             {children[0]}
-            <div
-                className="
-        container 
-        h-full w-full 
-        absolute    
-        top-0
-        left-0
-        z-50
-        opacity-100
-        items-center
-        rounded-md"
-            >
-                {(isVisible && children[1]) ?? null}
-            </div>
+            {isVisible && children[1] ? (
+                <div
+                    className="
+         container 
+         h-full w-full 
+         absolute    
+         top-0
+         left-0
+         z-50
+         opacity-100
+         items-center
+         rounded-md"
+                >
+                    {children[1]}
+                </div>
+            ) : null}
         </div>
     );
 }
