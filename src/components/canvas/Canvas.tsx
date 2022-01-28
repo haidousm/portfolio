adebb0999cd7c6e749c6502eaad3013ee5a33300
haidousm <haidous.m@gmail.com>
2022-01-28 17:07:23 +0200

    fix canvas package

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import CanvasDraw from "react-canvas-draw";
 
+interface BetterCanvasDraw extends CanvasDraw {
+    getDataURL(
+        fileType: string,
+        useBgImage: boolean,
+        backgroundColour: string
+    ): string;
+}
+
 function Canvas() {
+    const [canvas, setCanvas] = useState<BetterCanvasDraw | null>(null);
     return (
         <CanvasDraw
+            ref={(canvasDraw) => setCanvas(canvasDraw as BetterCanvasDraw)}
+            onChange={() => {
+                console.log(canvas!.getDataURL("png", false, "#000000"));
+            }}
             gridColor="rgba(255, 255, 255, 1)"
             hideInterface={true}
             backgroundColor="#1e1e1e"
             hideGrid={true}
-            canvasWidth={"200px"}
+            brushColor="#ffffff"
+            canvasWidth={"100%"}
+            canvasHeight={"100%"}
+            className="rounded-md"
         />
     );
 }
--- src/components/fine-demo/FineDemo.tsx
+++ src/components/fine-demo/FineDemo.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Canvas from "../canvas/Canvas";
+import Terminal from "../terminal/Terminal";
 
 function FineDemo() {
     return (
         <div
-            className="h-96 w-96 shadow-xl-heavy rounded-md m-4 lg:w-1/2 border border-mac-gray-74
+            className="h-96 shadow-xl-heavy rounded-md m-4 w-3/4 lg:w-1/3 border border-mac-gray-74
 "
         >
-            <Canvas />
+            <Terminal
+                statusBarHTML={
+                    <p className="text-center text-sm">Prediction: --</p>
+                }
+            >
+                <Canvas />
+            </Terminal>
         </div>
     );
 }
