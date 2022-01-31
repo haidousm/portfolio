44a0e234ee18d60058f2aaa54cb334cdb52faeea
haidousm <haidous.m@gmail.com>
2022-01-31 21:41:39 +0200

    add post route to update social-card

--- src/pages/api/social-card/index.ts
+++ src/pages/api/social-card/index.ts
@@ -17,15 +17,13 @@ export interface BetterSocket extends Socket {
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "GET") {
-        const proto =
-            req.headers["x-forwarded-proto"] ||
-            (req.socket as BetterSocket).encrypted
-                ? "https"
-                : "http";
-        const response = await axios.get(
-            `${proto}://${req.headers.host}/social-card.json`
-        );
-        const card = response.data as SocialMediaCard;
+        const card = (await fsp.readFile(
+            "./public/social-card.json"
+        )) as SocialMediaCard;
+        res.status(200).json(card);
+    } else if (req.method === "POST") {
+        const card = req.body as SocialMediaCard;
+        await fsp.writeFile("./public/social-card.json", JSON.stringify(card));
         res.status(200).json(card);
     }
 };
